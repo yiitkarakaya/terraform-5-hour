f0ab88b5605a30170a5f295f3ab78bf1fb9720a2
farrukh90 <farrukhsadykov@gmail.com>
2022-06-01 17:34:51 +0000

    added localscript

---
+++ localscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git clone https://github.com/tfutils/tfenv.git ~/.tfenv
+echo 'export PATH="$HOME/.tfenv/bin:$PATH"' >> ~/.bash_profile
+sudo ln -s ~/.tfenv/bin/* /usr/local/bin
+
+# bash localscript.sh
+tfenv
