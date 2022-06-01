34266c335728c00477ffbce0b01d82eebafef302
farrukh90 <farrukhsadykov@gmail.com>
2022-06-01 18:44:19 +0000

    formatted

--- startup/main.tf
+++ startup/main.tf
@@ -23,8 +23,6 @@ resource "aws_subnet" "public3" {
   tags                    = var.tags
   map_public_ip_on_launch = true
 }
-
-
 resource "aws_subnet" "private1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.private_subnet1
@@ -49,15 +47,13 @@ resource "aws_internet_gateway" "gw" {
   tags   = var.tags
 }
 
-
-
 resource "aws_route_table" "example" {
   vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
-  tags   = var.tags
+  tags = var.tags
 }
 
 
@@ -65,14 +61,10 @@ resource "aws_route_table_association" "public1" {
   subnet_id      = aws_subnet.public1.id
   route_table_id = aws_route_table.example.id
 }
-
-
 resource "aws_route_table_association" "public2" {
   subnet_id      = aws_subnet.public2.id
   route_table_id = aws_route_table.example.id
 }
-
-
 resource "aws_route_table_association" "public3" {
   subnet_id      = aws_subnet.public3.id
   route_table_id = aws_route_table.example.id
