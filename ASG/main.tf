005f1f43a7013ca92bf1502c74a195ffe5e20484
farrukh90 <farrukhsadykov@gmail.com>
2022-06-01 20:32:36 +0000

    added elb

--- ASG/main.tf
+++ ASG/main.tf
@@ -13,5 +13,36 @@ resource "aws_autoscaling_group" "bar" {
   launch_template {
     id      = aws_launch_template.foobar.id
     version = "$Latest"
+    
   }
+}
+
+
+resource "aws_elb" "bar" {
+  name               = "foobar-terraform-elbs"
+  availability_zones = ["us-east-1a"]
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+}
+
+
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = aws_autoscaling_group.bar.id
+  elb                    = aws_elb.bar.id
 }
