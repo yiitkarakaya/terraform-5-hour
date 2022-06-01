aff82d246ef451f3785627cf59b215ce80601a55
farrukh90 <farrukhsadykov@gmail.com>
2022-06-01 17:56:44 +0000

    added fmt

---
+++ startup/main.tf
@@ -0,0 +1,6 @@
+resource "aws_vpc" "main" {
+  cidr_block           = var.cidr_block
+  instance_tenancy     = "default"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
---
+++ startup/variable.tf
@@ -0,0 +1,10 @@
+variable "cidr_block" {
+  description = "Please provide CIDR block for VPC"
+  type        = string
+  default     = ""
+}
+variable "region" {
+  description = "Please provide a region name"
+  type        = string
+  default     = ""
+}
